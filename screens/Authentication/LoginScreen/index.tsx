341f84f4b87e4607446b5c65b76106b16a2ca701
Ankur Ahir <ahir.ankur0429@gmail.com>
2023-09-06 23:37:50 -0700

    keyboard good enough

--- screens/Authentication/LoginScreen/index.tsx
+++ screens/Authentication/LoginScreen/index.tsx
@@ -7,9 +7,10 @@ import {
   Divider,
   HStack,
   Pressable,
+  KeyboardAvoidingView,
 } from 'native-base';
 import {useRef} from 'react';
-import {TextInput} from 'react-native';
+import {Platform, TextInput} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import useColorScheme from '../../../hooks/useColorScheme';
@@ -21,67 +22,72 @@ const LoginScreen = () => {
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <VStack space={5} px={5} pt={5} h="full" maxW="450px">
-        <Spacer />
-        <Text fontSize="3xl" fontWeight="bold">
-          Log In
-        </Text>
-        <Input
-          placeholder="Email Address"
-          returnKeyType="next"
-          onSubmitEditing={() => {
-            PasswordRef.current && PasswordRef.current.focus();
-          }}
-        />
-        <Input
-          placeholder="Password"
-          returnKeyType="done"
-          ref={PasswordRef}
-          secureTextEntry
-        />
-        <Button
-          borderRadius={15}
-          h={12}
-          _text={{fontSize: 18}}
-          color="constants.primary">
-          Login
-        </Button>
-
-        <HStack alignSelf="center" my={3}>
-          <Divider
-            bg={currTheme + '.text'}
-            thickness="1"
-            my="auto"
-            mx="2"
-            w="1/3"
-            bgColor="constants.greyText"
-            orientation="horizontal"
+      <KeyboardAvoidingView
+        flex={1}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <VStack space={5} px={5} pt={5} h="full" maxW="450px">
+          <Spacer />
+          <Text fontSize="3xl" fontWeight="bold">
+            Log In
+          </Text>
+          <Input
+            placeholder="Email Address"
+            returnKeyType="next"
+            blurOnSubmit={false}
+            onSubmitEditing={() => {
+              PasswordRef.current && PasswordRef.current.focus();
+            }}
           />
-          <Text color="constants.greyText">or</Text>
-          <Divider
-            bg={currTheme + '.text'}
-            thickness="1"
-            my="auto"
-            mx="2"
-            w="1/3"
-            bgColor="constants.greyText"
-            orientation="horizontal"
+          <Input
+            placeholder="Password"
+            returnKeyType="done"
+            ref={PasswordRef}
+            secureTextEntry
           />
-        </HStack>
+          <Button
+            borderRadius={15}
+            h={12}
+            _text={{fontSize: 18}}
+            color="constants.primary">
+            Login
+          </Button>
 
-        <AuthButtons />
+          <HStack alignSelf="center" my={3}>
+            <Divider
+              bg={currTheme + '.text'}
+              thickness="1"
+              my="auto"
+              mx="2"
+              w="1/3"
+              bgColor="constants.greyText"
+              orientation="horizontal"
+            />
+            <Text color="constants.greyText">or</Text>
+            <Divider
+              bg={currTheme + '.text'}
+              thickness="1"
+              my="auto"
+              mx="2"
+              w="1/3"
+              bgColor="constants.greyText"
+              orientation="horizontal"
+            />
+          </HStack>
 
-        <Spacer />
+          <AuthButtons />
 
-        <HStack alignSelf="center" space={1}>
-          <Text>Don't have an account?</Text>
-          <Pressable onPress={() => {}}>
-            <Text color="constants.primary" fontWeight={600}>
-              Sign Up
-            </Text>
-          </Pressable>
-        </HStack>
-      </VStack>
+          <Spacer />
+
+          <HStack alignSelf="center" space={1}>
+            <Text>Don't have an account?</Text>
+            <Pressable onPress={() => {}}>
+              <Text color="constants.primary" fontWeight={600}>
+                Sign Up
+              </Text>
+            </Pressable>
+          </HStack>
+        </VStack>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
